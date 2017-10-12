5994b0182ee5a8bd6b80bf2fa859efaf19144b75
Michele Colombo <mikexine@gmail.com>
2017-10-12 21:31:50 +0200

    predictions

---
+++ predict.py
@@ -0,0 +1,41 @@
+# Pandas are predefined data structures that provide nice features for data handling
+import pandas as pd
+
+# Models from sklearn - KNN
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+
+# Read data, by specifying the file, delimiter and the header row
+data = pd.read_csv('export.csv', sep=',', header=0)
+
+# Print a short summary of the dataset
+print(data.describe())
+
+# Separate the independent (predictor) variables and the dependent variables (predicted)
+x_data = data.filter(['comment_count','follows','followed_by','python_faces'], axis=1).fillna(0)
+y_data = data.filter(['like_count'], axis=1).fillna(0)
+
+# Split the dataset into train and test with a 80-20 ratio
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2)
+
+# Create a KNN regressor from sklearn
+neigh = KNeighborsRegressor(n_neighbors=15)
+
+# Train the model
+neigh.fit(x_train, y_train)
+
+# Display the R squared score of the match between predicted and true value
+print(neigh.score(x_test, y_test))
+
+# Generate the predicted column of data
+y_predict = neigh.predict(x_test)
+
+# Store it in a panda dataframe for easier comparison with the true values
+y_predict = pd.DataFrame(y_predict, y_test.index, columns=['predicted_like_count'])
+
+# Concatenate the two columns for comparison
+y_analysis = pd.concat([y_predict, y_test], axis=1)
+
+# Display the two columns
+print(y_analysis)
+
